0827fe287b3dab6193628aa87471fd2f6588082c
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-06-30 21:46:56 -0300

    closes #1 - Pacotes e esqueleto pronto

---
+++ exemplos/exemplo03-quantity-array/js/javascript.js
@@ -0,0 +1,34 @@
+var prods = [
+    { id: 1, name: "Produto 1", price: 10.0 },
+    { id: 2, name: "Produto 2", price: 20.0 },
+];
+
+function calc(){
+    var quantities = document.getElementsByName("quantity");
+    var output     = document.getElementById("output");
+    var total      = 0;
+    
+    output.innerHTML = "";
+
+    for (var input of quantities) {
+        var id = input.id;
+
+        output.innerHTML += `Produto: ${prods[id-1].name}  - Preço: ${prods[id-1].price} - Quantidade: ${input.value} </br>`;
+        total            += prods[id-1].price * parseFloat(input.value);
+    
+    
+    }
+
+    var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
+    output.innerHTML += `Preço: ${formatter.format(prods[0].price)}</br>`
+    output.innerHTML += `Preço: ${formatter.format(prods[1].price)}</br>`
+
+    output.innerHTML += `<h2>Total: ${total}</h2>`;
+
+
+}
+
